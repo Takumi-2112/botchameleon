052ea130c0ea026351615c2f2dd257c516ce761a
Tommy <azran.thomas@gmail.com>
2025-06-29 12:45:43 -0400

    render live backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -37,7 +37,7 @@ function App() {
     setInput("");
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/message", {
+      const res = await fetch("https://botchameleon.onrender.com/message", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
