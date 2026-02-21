e78fee4281dc63a1b1884e3202709d17c15ecc07
Tommy <azran.thomas@gmail.com>
2026-02-21 15:39:24 -0500

    fixing host

--- server.py
+++ server.py
@@ -34,4 +34,4 @@ def message():
 
 if __name__ == '__main__':
     # app.run()
-    app.run(debug=True, host='', port=5000)  # Run on all interfaces for local testing
+    app.run(debug=True, host='0.0.0.0', port=5000)  # Run on all interfaces for local testing
